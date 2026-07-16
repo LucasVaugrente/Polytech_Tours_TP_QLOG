4ce45a132848bf5adcad5f979a2feec83bd7330d
Lucas Vaugrente <lucas.vaugrente@worldline.com>
2026-07-16 15:36:43 +0200

    add config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2022: true, node: true },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended'],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:cypress/recommended'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   rules: {
     'vue/multi-word-component-names': 'off',
